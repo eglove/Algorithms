6b33d8524ddf39d85dbb128e4f1fd1e516a4dca1
eglove <glover.ethan@gmail.com>
2020-08-19 20:27:54 -0500

    more files

---
+++ containsDuplicate.js
@@ -0,0 +1,34 @@
+// Given an array of integers, find if the array contains any duplicates.
+
+// 	Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+// 	Example 1:
+// Input: [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+let example1 = [1,2,3,1];
+let example2 = [1,2,3,4];
+let example3 = [1,1,1,3,3,4,3,2,4,2];
+
+function containsDuplicates(nums) {
+	let map = new Map();
+	for(let i = 0; i < nums.length; i++) {
+		if(map.has(nums[i])) {
+			return true;
+		}
+		map.set(nums[i], i);
+	}
+
+	return false;
+}
+
+console.log(containsDuplicates(example1));
+console.log(containsDuplicates(example2));
+console.log(containsDuplicates(example3));
