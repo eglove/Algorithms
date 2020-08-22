8a25366792a5ddab09bff97d8de67f7783a4a1e9
eglove <glover.ethan@gmail.com>
2020-08-22 14:51:58 -0500

    linked list update

--- linkedList.js
+++ linkedList.js
@@ -17,20 +17,60 @@ class LinkedList {
 
 	append(value) {
 		const newNode = new Node(value);
-
 		this.tail.next = newNode;
 		this.tail = newNode;
 		this.length++;
-		console.log(this);
 	}
 
 	prepend(value) {
 		const newNode = new Node(value);
-
 		newNode.next = this.head;
 		this.head = newNode;
 		this.length++;
-		console.log(this);
+	}
+
+	printList() {
+		const array = [];
+		let currentNode = this.head;
+		while (currentNode !== null) {
+			array.push(currentNode.value);
+			currentNode = currentNode.next;
+		}
+		return array;
+	}
+
+	insert(index, value) {
+		if (index === 0) {
+			this.prepend(value);
+			return this.printList();
+		} else if (index >= this.length) {
+			return this.append(value);
+		}
+
+		const newNode = new Node(value);
+		const leader = this.traverseToIndex(index - 1);
+		const holdingPointer = leader.next;
+		leader.next = newNode;
+		newNode.next = holdingPointer;
+		this.length++;
+		return this.printList();
+	}
+
+	remove(index) {
+		const leader = this.traverseToIndex(index - 1);
+		const unwantedNode = leader.next;
+		leader.next = unwantedNode.next;
+		this.length--;
+	}
+
+	traverseToIndex(index) {
+		let counter = 0;
+		let currentNode = this.head;
+		while (counter !== index) {
+			currentNode = currentNode.next;
+			counter++;
+		}
+		return currentNode;
 	}
 }
 
@@ -38,3 +78,9 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+console.log(myLinkedList.printList());
+myLinkedList.remove(2);
+myLinkedList.remove(2);
+console.log(myLinkedList.printList());
