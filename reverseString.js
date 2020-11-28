6506960ebd0ee3fabb7160f906459ee0de6e568d
eglove <hello@ethang.email>
2020-11-28 16:02:45 -0600

    one more

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+const reverseString = function(s) {
+  return s.reverse();
+};
+
+// Input: ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+console.log(reverseString(["h","e","l","l","o"]))
+
+// Input: ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+console.log(reverseString(["H","a","n","n","a","h"]))
