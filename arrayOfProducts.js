019a6663387dff8dc21aba34773fcc3c4a4409da
Ethan Glover <hello@ethang.email>
2020-12-18 12:47:01 -0600

    array of products

--- arrayOfProducts.js
+++ arrayOfProducts.js
@@ -2,10 +2,10 @@
 function arrayOfProducts(array) {
     let products = [];
 
-    for(let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         let product = 1;
         for (j = 0; j < array.length; j++) {
-            if(i !== j) {
+            if (i !== j) {
                 product *= array[j];
             }
         }
@@ -17,20 +17,32 @@ function arrayOfProducts(array) {
 
 // O(n) Time - O(n) Space
 function arrayOfProducts2(array) {
-    let products = [];
+    const products = new Array(array.length).fill(1);
 
-    let totalProduct = 1;
-    array.forEach(num => {
-        totalProduct *= num;
-    })
+    let leftProduct = 1;
+    for (let i = 0; i < array.length; i++) {
+        products[i] = leftProduct;
+        leftProduct *= array[i];
+    }
 
-    array.forEach(num => {
-        products.push(totalProduct / num);
-    })
+    let rightProduct = 1;
+    for (let i = array.length - 1; i >= 0; i--) {
+        products[i] *= rightProduct;
+        rightProduct *= array[i];
+    }
 
     return products;
 }
 
 let array = [5, 1, 4, 2];
 console.log(arrayOfProducts(array));
+console.log(arrayOfProducts2(array));
+
+array = [0, 0, 0, 0];
+console.log(arrayOfProducts2(array));
+
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arrayOfProducts2(array));
+
+array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 console.log(arrayOfProducts2(array));
